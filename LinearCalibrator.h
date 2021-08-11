c24a35812f9439cf93820f889024a2342ffee19b
Benjamin HENRY <47706397+benjamin-henry@users.noreply.github.com>
2021-08-11 13:30:43 +0200

    Update LinearCalibrator.h

--- LinearCalibrator.h
+++ LinearCalibrator.h
@@ -1,17 +1,11 @@
 #ifndef LINEAR_CALIBRATOR_H
 #define LINEAR_CALIBRATOR_H
 
-#define CALIB_FACTOR 16383
-#define VCC 3.3
-#define STEP VCC/CALIB_FACTOR
-
 #include <stdint.h>
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 typedef struct adc_params_t {
     float vcc;
@@ -26,16 +20,13 @@ void set_adc_params(ADC_PARAMS adc_params, float vcc, uint32_t resolution, int32
 
 //output params gain and offset [a, b];
 void resolve_params(int32_t *sensor_inputs, int32_t *read_values, int32_t *target_values, int32_t *output_params, ADC_PARAMS adc_params);
-
 void resolve_shift(int32_t *temperatures, int32_t *params_low, int32_t *params_high, int32_t *gain_params, int32_t *offset_params, ADC_PARAMS adc_params);
 
 void compensate_params(int32_t temperature, int32_t *gain_params, int32_t *offset_params, int32_t *compensated_params, ADC_PARAMS adc_params);
 int32_t compensate_sample(int32_t sample, int32_t temperature, int32_t *gain_params, int32_t *offset_params, int32_t *compensated_params, ADC_PARAMS adc_params);
 
-
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif
